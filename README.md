34b47f07b8faee0117d50f3f3a9b0b093442da8c
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-25 20:33:05 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Storage DSLink
+
+A Key:Value Storage System for DSA.
+
+## Usage
+
+```bash
+pub get
+dart bin/run.dart
+```
---
+++ bin/run.dart
@@ -0,0 +1,125 @@
+import "package:dslink/client.dart";
+import "package:dslink/responder.dart";
+
+LinkProvider link;
+
+main(List<String> args) async {
+  link = new LinkProvider(args, "Storage-", command: "run", defaultNodes: {
+    "Create Bucket": {
+      r"$is": "createBucket",
+      r"$invokable": "write",
+      r"$result": "values",
+      r"$params": [
+        {
+          "name": "name",
+          "type": "string"
+        }
+      ],
+      r"$columns": []
+    }
+  }, profiles: {
+    "createBucket": (String path) => new CreateBucketNode(path),
+    "deleteBucket": (String path) => new DeleteNode(path, 1),
+    "createEntry": (String path) => new CreateEntryNode(path),
+    "deleteEntry": (String path) => new DeleteNode(path, 2)
+  });
+
+  if (link.link == null) return;
+
+  link.connect();
+}
+
+class CreateBucketNode extends SimpleNode {
+  CreateBucketNode(String name) : super(name);
+
+  @override
+  Object onInvoke(Map<String, dynamic> params) {
+    if (params["name"] == null) return {};
+
+    var name = params["name"];
+
+    link.provider.addNode("/${name}", {
+      "Create Entry": {
+        r"$is": "createEntry",
+        r"$invokable": "write",
+        r"$result": "values",
+        r"$params": [
+          {
+            "name": "key",
+            "type": "string"
+          },
+          {
+            "name": "type",
+            "type": "string"
+          }
+        ],
+        r"$columns": []
+      },
+      "Delete Bucket": {
+        r"$is": "deleteBucket",
+        r"$invokable": "write",
+        r"$result": "values",
+        r"$params": [],
+        r"$columns": []
+      }
+    });
+
+    link.save();
+    return {};
+  }
+}
+
+class CreateEntryNode extends SimpleNode {
+  CreateEntryNode(String path) : super(path);
+
+  @override
+  Object onInvoke(Map<String, dynamic> params) {
+    if (params["key"] == null || params["type"] == null) {
+      return {};
+    }
+
+    var key = params["key"];
+    var type = params["type"];
+
+    SimpleNode holder = link.provider.getNode(path.split("/").take(2).join("/"));
+    link.provider.addNode("${holder.path}/${key}", {
+      "Delete Entry": {
+        r"$is": "deleteEntry",
+        r"$invokable": "write",
+        r"$result": "values",
+        r"$params": [],
+        r"$columns": []
+      },
+      r"$type": type,
+      r"$writable": "write",
+      "?value": null
+    });
+
+    listeners["${holder.path}/${key}"] = (link.provider.getNode("${holder.path}/${key}") as SimpleNode).subscribe((value) {
+      link.save();
+    });
+
+    link.save();
+    return {};
+  }
+}
+
+Map<String, RespSubscribeListener> listeners = {};
+
+class DeleteNode extends SimpleNode {
+  final int parts;
+
+  DeleteNode(String path, this.parts) : super(path);
+
+  @override
+  Object onInvoke(Map<String, dynamic> params) {
+    var p = path.split("/").take(parts + 1).join("/");
+    link.provider.removeNode(p);
+    if (listeners.containsKey(p)) {
+      listeners[p].cancel();
+      listeners.remove(p);
+    }
+    link.save();
+    return {};
+  }
+}
