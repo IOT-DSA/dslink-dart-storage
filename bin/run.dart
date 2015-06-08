ee4fc0c1a3f27a2db34612010eae8552f3d5c8a1
Kenneth Endfinger <kaendfinger@gmail.com>
2015-06-08 12:06:15 -0400

    Work on Rewrite

--- bin/run.dart
+++ bin/run.dart
@@ -1,11 +1,65 @@
-import "package:dslink/client.dart";
-import "package:dslink/responder.dart";
+import "dart:async";
+
+import "package:dslink/dslink.dart";
+import "package:dslink/nodes.dart";
 
 LinkProvider link;
 
 main(List<String> args) async {
   link = new LinkProvider(args, "Storage-", command: "run", defaultNodes: {
-    "Create Bucket": {
+    r"$is": "bucket"
+  }, profiles: {
+    "createBucket": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) {
+      var name = params["name"];
+      var x = new Path(path).parentPath;
+      var t = "${x}/${name}";
+      link.addNode(t, {
+        r"$is": "bucket"
+      });
+      link.save();
+    }),
+    "delete": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) {
+      var p = new Path(path).parentPath;
+      link.removeNode(p);
+      if (listeners.containsKey(p)) {
+        listeners[p].cancel();
+        listeners.remove(p);
+      }
+      link.save();
+      return {};
+    }),
+    "createEntry": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) {
+      var name = params["name"];
+      var type = params["type"];
+
+      var p = "${new Path(path).parentPath}/${name}";
+
+      link.addNode(p, {
+        r"$is": "entry",
+        r"$type": type,
+        r"$writable": "write"
+      });
+
+      link.save();
+    }),
+    "bucket": (String path) => new BucketNode(path),
+    "entry": (String path) => new EntryNode(path)
+  });
+
+  link.connect();
+}
+
+class BucketNode extends SimpleNode {
+  BucketNode(String path) : super(path);
+
+  @override
+  onCreated() {
+    link.removeNode("${path}/Create_Bucket");
+    link.removeNode("${path}/Create_Entry");
+    link.removeNode("${path}/Delete_Bucket");
+
+    link.addNode("${path}/Create_Bucket", {
+      r"$name": "Create Bucket",
       r"$is": "createBucket",
       r"$invokable": "write",
       r"$result": "values",
@@ -16,110 +70,82 @@ main(List<String> args) async {
         }
       ],
       r"$columns": []
-    }
-  }, profiles: {
-    "createBucket": (String path) => new CreateBucketNode(path),
-    "deleteBucket": (String path) => new DeleteNode(path, 1),
-    "createEntry": (String path) => new CreateEntryNode(path),
-    "deleteEntry": (String path) => new DeleteNode(path, 2)
-  });
-
-  if (link.link == null) return;
-
-  link.connect();
-}
-
-class CreateBucketNode extends SimpleNode {
-  CreateBucketNode(String name) : super(name);
-
-  @override
-  Object onInvoke(Map<String, dynamic> params) {
-    if (params["name"] == null) return {};
-
-    var name = params["name"];
-
-    link.addNode("/${name}", {
-      "Create Entry": {
-        r"$is": "createEntry",
-        r"$invokable": "write",
-        r"$result": "values",
-        r"$params": [
-          {
-            "name": "key",
-            "type": "string"
-          },
-          {
-            "name": "type",
-            "type": "string"
-          }
-        ],
-        r"$columns": []
-      },
-      "Delete Bucket": {
-        r"$is": "deleteBucket",
-        r"$invokable": "write",
-        r"$result": "values",
-        r"$params": [],
-        r"$columns": []
-      }
     });
 
-    link.save();
-    return {};
+    link.addNode("${path}/Create_Entry", {
+      r"$name": "Create Entry",
+      r"$is": "createEntry",
+      r"$invokable": "write",
+      r"$result": "values",
+      r"$params": [
+        {
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "name": "type",
+          "type": buildEnumType([
+            "string",
+            "number",
+            "bool",
+            "color",
+            "gradient",
+            "fill",
+            "array",
+            "map"
+          ])
+        }
+      ],
+      r"$columns": []
+    });
+
+    link.addNode("${path}/Delete_Bucket", {
+      r"$name": "Delete Bucket",
+      r"$is": "delete",
+      r"$invokable": "write",
+      r"$result": "values",
+      r"$params": [],
+      r"$columns": []
+    });
+  }
+
+  @override
+  Map save() {
+    var x = super.save();
+    x.remove("Create_Bucket");
+    x.remove("Create_Entry");
+    x.remove("Delete_Bucket");
+    return x;
   }
 }
 
-class CreateEntryNode extends SimpleNode {
-  CreateEntryNode(String path) : super(path);
+class EntryNode extends SimpleNode {
+  EntryNode(String path) : super(path);
 
   @override
-  Object onInvoke(Map<String, dynamic> params) {
-    if (params["key"] == null || params["type"] == null) {
-      return {};
-    }
+  onCreated() {
+    link.removeNode("${path}/Delete_Entry");
 
-    var key = params["key"];
-    var type = params["type"];
-
-    SimpleNode holder = link.provider.getNode(path.split("/").take(2).join("/"));
-    link.provider.addNode("${holder.path}/${key}", {
-      "Delete Entry": {
-        r"$is": "deleteEntry",
-        r"$invokable": "write",
-        r"$result": "values",
-        r"$params": [],
-        r"$columns": []
-      },
-      r"$type": type,
-      r"$writable": "write",
-      "?value": null
+    link.addNode("${path}/Delete_Entry", {
+      r"$name": "Delete Entry",
+      r"$is": "delete",
+      r"$invokable": "write",
+      r"$result": "values",
+      r"$params": [],
+      r"$columns": []
     });
 
-    listeners["${holder.path}/${key}"] = (link.provider.getNode("${holder.path}/${key}") as SimpleNode).subscribe((value) {
+    listeners[path] = link.onValueChange(path).listen((x) {
       link.save();
     });
-
-    link.save();
-    return {};
   }
-}
-
-Map<String, RespSubscribeListener> listeners = {};
-
-class DeleteNode extends SimpleNode {
-  final int parts;
-
-  DeleteNode(String path, this.parts) : super(path);
 
   @override
-  Object onInvoke(Map<String, dynamic> params) {
-    var p = path.split("/").take(parts + 1).join("/");
-    link.removeNode(p);
-    if (listeners.containsKey(p)) {
-      listeners[p].cancel();
-      listeners.remove(p);
-    }
-    link.save();
-    return {};
+  Map save() {
+    var x = super.save();
+    x.remove("Delete_Entry");
+    return x;
   }
 }
+
+Map<String, StreamSubscription> listeners = {};
